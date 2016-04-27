860bda79cbec95bbcb910cac56f0a8c9cb73d13f
张云龙 <yunlong.zhang@heysapp.com>
2016-04-27 16:50:28 +0800

    添加promise版本，避免回调地狱。

--- redis/node_redis/test.js
+++ redis/node_redis/test.js
@@ -7,6 +7,7 @@
  * @type {User|exports|module.exports}
  */
 var User = require('./model');
+var Q = require('q');
 
 
 /**
@@ -24,39 +25,72 @@ var testUsers = {
 /**
  * 用于创建用户的函数
  * */
-function create(users, fn){
-    var total = Object.keys(users).length;
+function create(users){
+    console.log('test++');
+    var defer = Q.defer();
+    var defers = [];
+    //var total = Object.keys(users).length;
     for (var i in users){
         (function (email, data) {
+            var defer = Q.defer();
             var user = new User(email,data);
             user.save(function (err) {
-                if (err) throw err;
-                --total || fn();
+                if (err) defer.reject(err);
+                //--total || fn();
+                defer.resolve('create ' + email + ' sucess!');
             });
+            defers.push(defer.promise);
         })(i,users[i]);
     }
+
+    Q.all(defers)
+        .then(function (data) {
+            for(var i in data){
+                console.log(data[i]);
+            }
+            defer.resolve(users);
+        }, console.error);
+
+    return defer.promise;
 }
 
 /**
  * 用于水合用户的函数
  * */
-function hydrate (users, fn){
-    var total = Object.keys(users).length;
+function hydrate (users){
+    var defer = Q.defer();
+    var defers = [];
+    //var total = Object.keys(users).length;
     for(var i in users){
         (function (email) {
+            var defer = Q.defer();
             User.find(email, function (err, user) {
-                if(err) throw err;
+                if(err) defer.reject(err);
                 users[email] = user;
-                --total || fn();
+                defer.resolve('hydrate user: ' + email + ' success.');
+                //--total || fn();
             });
+            defers.push(defer.promise);
         })(i);
     }
+
+    Q.all(defers)
+        .then(function (data) {
+            for(var i in data){
+                console.log(data[i]);
+            }
+            defer.resolve(users);
+        },console.error);
+
+    return defer.promise;
 }
 
 /**
  * 创建测试用户
  * */
-create(testUsers, function () {
+
+//回调地狱版本
+/*create(testUsers, function () {
     hydrate(testUsers, function () {
         testUsers['bill@microsoft.com'].follow('jeff@amazon.com', function (err) {
             if(err) throw err;
@@ -85,4 +119,59 @@ create(testUsers, function () {
             });
         });
     });
-});
+});*/
+
+
+//Promise版本,非常漂亮的链式结构,当然可以用Q.denodeify简化
+create(testUsers)
+    .then(hydrate,console.error)
+    .then(function (testUsers) {
+        var defer = Q.defer();
+        testUsers['bill@microsoft.com'].follow('jeff@amazon.com', function (err) {
+            if(err) defer.reject(err);
+            console.log('+ bill followed jeff');
+            defer.resolve(testUsers);
+        });
+        return defer.promise;
+    },console.error)
+    .then(function (testUsers) {
+        var defer = Q.defer();
+        testUsers['jeff@amazon.com'].getFollowers(function (err, users) {
+            if(err) defer.reject(err);
+            console.log("jeff's followers",users);
+            defer.resolve(testUsers);
+        });
+        return defer.promise;
+    },console.error)
+    .then(function (testUsers) {
+        var defer = Q.defer();
+        testUsers['jeff@amazon.com'].getFriends(function (err, users) {
+            if(err) defer.reject(err);
+            console.log("jeff's friends",users);
+            defer.resolve(testUsers);
+        });
+        return defer.promise;
+    },console.error)
+    .then(function (testUsers) {
+        var defer = Q.defer();
+        testUsers['jeff@amazon.com'].follow('bill@microsoft.com', function (err) {
+            if(err) defer.reject(err);
+            console.log('+ jeff followed bill');
+            defer.resolve(testUsers);
+        });
+        return defer.promise;
+    },console.error)
+    .then(function (testUsers) {
+        var defer = Q.defer();
+        testUsers['jeff@amazon.com'].getFriends(function (err, users) {
+            if(err) defer.reject(err);
+            console.log("jeff's friends",users);
+            defer.resolve('done.');
+        });
+        return defer.promise;
+    },console.error)
+    .done(function (data) {
+        console.log(data);
+        process.exit();
+    });
+
